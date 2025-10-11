5ffeebcd9af25fcbdb990105b13a05062dce8eb8
Juan Teran <jupate2004@gmail.com>
2025-10-10 19:33:34 -0500

    Router Actualizado

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,28 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import LoginEmailView from '@/views/LoginEmailView.vue';
-//import TabsPage from '../views/TabsPage.vue'
+import { createRouter, createWebHistory } from '@ionic/vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+import TabsPage from '@/views/TabsPage.vue'
+
+import HomePage from '@/views/HomePage.vue'
+import RegisterPage from '@/views/RegisterPage.vue'
+import LoginEmailView from '@/views/LoginEmailView.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  { path: '/', redirect: '/home' },
+
+  { path: '/home', name: 'home', component: HomePage },
+  { path: '/login', name: 'login', component: LoginEmailView },
+  { path: '/register', name: 'register', component: RegisterPage },
+
   {
-    path: '/',
-    redirect: '/login'
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      { path: '', redirect: '/tabs/tab1' },
+      { path: 'tab1', component: () => import('@/views/Tab1Page.vue') },
+      { path: 'tab2', component: () => import('@/views/Tab2Page.vue') },
+      { path: 'tab3', component: () => import('@/views/Tab3Page.vue') },
+    ]
   },
-  {
-    path: '/login',
-    component: LoginEmailView
-  }
 ]
 
 const router = createRouter({
